34540b41a4045dfcc7050750ca0538a3813929d1
Tanel Sitska <TanelS@users.noreply.github.com>
2018-08-08 19:36:14 +0300

    First candidate for submission

---
+++ README.md
@@ -0,0 +1,22 @@
+# Restaurant Review App
+
+## Background
+
+The Restaurant review app was done as a part of the Udacity Front-End Developer Nanodegree course.
+
+
+## Instructions
+
+Download the `.zip` file to your computer or clone it:
+
+```git clone https://github.com/TanelS/Restaurant-Reviews-App```
+
+If you have Python installed run a simple local webserver:
+
+- In a terminal, check the version of Python you have: python -V. If you have Python 2.x, spin up the server with python -m SimpleHTTPServer 8000 (or some other port, if port 8000 is already in use.) For Python 3.x, you can use python3 -m http.server 8000. If you don't have Python installed, navigate to Python's website to download and install the software.
+
+- When server is running visit the URL `http://localhost:8000`
+
+It should be noted that Python webserver can behave badly at times, SHIFT-_reload_ helps in most cases. 
+
+App is optimized for the desktop, Google Nexus 5 and Apple iPad.
---
+++ sw.js
@@ -0,0 +1,56 @@
+var staticCacheName = 'restaurant-static';
+
+self.addEventListener('install', function(event) {
+    event.waitUntil(
+        caches.open(staticCacheName).then(function(cache) {
+            return cache.addAll([
+                '/',
+                '/index.html',
+                '/css/styles.css',
+                '/restaurant.html',
+                '/data/restaurants.json',
+                '/img/1.jpg',
+                '/img/2.jpg',
+                '/img/3.jpg',
+                '/img/4.jpg',
+                '/img/5.jpg',
+                '/img/6.jpg',
+                '/img/7.jpg',
+                '/img/8.jpg',
+                '/img/9.jpg',
+                '/img/10.jpg',
+                '/js/dbhelper.js',
+                '/js/main.js',
+                '/js/restaurant_info.js'
+            ]);
+        })
+    );
+});
+
+self.addEventListener('activate', function(event) {
+    event.waitUntil(
+        caches.keys()
+            .then(function(cacheNames) {
+            return Promise.all(
+                cacheNames.filter(function(cacheName) {
+                    return cacheName.startsWith('restaurant-') &&
+                        cacheName !== staticCacheName;
+                }).map(function(cacheName) {
+                    return caches.delete(cacheName);
+                })
+            );
+        })
+    );
+});
+
+// get fetch query data from cache  and if the cache does not contain the data
+// then directly from the web
+self.addEventListener('fetch', function(event) {
+    event.respondWith(
+        caches.match(event.request)
+            .then(function(response) {
+            return response || fetch(event.request);
+        })
+    );
+});
+
